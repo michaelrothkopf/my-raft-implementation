99bb81d73494f16aa8f13669a33441158afb6b0b
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-08 19:20:32 -0400

    moved memory persister to raft to avoid circular import even though it is a simulated test persister

---
+++ raft-kv/raft/memory_persister.go
@@ -0,0 +1,63 @@
+package raft
+
+import (
+	"sync"
+)
+
+// MemoryPersister implements PersistenceProvider
+// Provides a store for simple memory holding of the persistent fields
+type MemoryPersister struct {
+	mu		sync.Mutex
+	state	PersistentState
+}
+
+// NewMemoryPersister constructs a MemoryPersister
+func NewMemoryPersister() *MemoryPersister {
+	return &MemoryPersister{state: PersistentState{CurrentTerm:0,VotedFor:-1,Log:nil}}
+}
+
+// Save saves new persistent state to the memory persister
+func (mp *MemoryPersister) Save(state PersistentState) error {
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+
+	// Deep copy the log and its entries
+	logCopy := make([]LogEntry, len(state.Log))
+	for i, logEntry := range state.Log {
+		logCopy[i] = LogEntry{
+			Term: logEntry.Term,
+			Index: logEntry.Index,
+			Command: append([]byte(nil), logEntry.Command...),
+		}
+	}
+	mp.state = PersistentState{
+		CurrentTerm: state.CurrentTerm,
+		VotedFor: state.VotedFor,
+		Log: logCopy,
+	}
+
+	return nil
+}
+
+// Load loads the persistent state from the memory persister
+func (mp *MemoryPersister) Load() (PersistentState, error) {
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+
+	// Deep copy the log and its entries
+	logCopy := make([]LogEntry, len(mp.state.Log))
+	for i, logEntry := range mp.state.Log {
+		logCopy[i] = LogEntry{
+			Term: logEntry.Term,
+			Index: logEntry.Index,
+			Command: append([]byte(nil), logEntry.Command...),
+		}
+	}
+	state := PersistentState{
+		CurrentTerm: mp.state.CurrentTerm,
+		VotedFor: mp.state.VotedFor,
+		Log: logCopy,
+	}
+
+	return state, nil
+}
